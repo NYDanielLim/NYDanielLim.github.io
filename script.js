1645669507082470088233119f6251a5492caef8
NYDanielLim <lim.daniel.10@gmail.com>
2018-08-27 18:07:08 -0400

    Complete Rework
    -Changed Styling of entire page
    -Added intro text
    -Added js and jquery applications

---
+++ script.js
@@ -0,0 +1,8 @@
+function scrollToProjects () {
+    var position = $("#projectBox").position();
+    window.scrollTo({top: position.top, behavior: "smooth"});
+}
+function scrollToExperience () {
+    var position = $("#experienceBox").position();
+    window.scrollTo({top: position.top, behavior: "smooth"});
+}
